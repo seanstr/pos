3be92112a82f37e99edd12836747c1ad5f2310a4
Sean Matthews <seanstr@gmail.com>
2017-08-27 19:58:03 -0600

    changed transactions to cards. some issues with saving the relations and resetting totals

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -1,61 +1,87 @@
 <template>
   <div>
-  <q-card v-if="page == 'transactions'" v-for="tx in tfpData.transactions" v-bind:currTx="tx" :key="tx.id" id="transactions">
-    <div :id="'btn'+tx.id" class="card-content">
-      <div class="row tx-summary-title text-italic light-paragraph item-delimiter" style="background-color: #dadada;">
-        <div class="col-1">Tx#</div>
-        <div class="col-2">Time</div>
-        <div class="col-2">Sum</div>
-        <div class="col-2">Tax</div>
-        <div class="col-2">Total</div>
-        <div class="col">TxType</div>
-      </div>
-      <div class="row tx-summary">
-        <div class="col-1">{{tx.id}}</div>
-        <div class="col-2">{{('0' + new Date(tx.txTime).getHours()).slice(-2) + ':' + ('0' + new Date(tx.txTime).getMinutes()).slice(-2)}}</div>
-        <div class="col-2"><div class="auto">{{tx.total}}</div></div>
-        <div class="col-2"><div class="auto">{{tx.tax}}</div></div>
-        <div class="col-2"><div class="auto">{{tx.total + tx.tax}}</div></div>
-        <div class="col">{{tx.pp_or_pl}}</div>
-      </div>
-      <div class="row tx-summary" />
-      <q-card-separator />
-      <q-card-separator />
-      <q-card-separator />
-      <q-card-separator />
-      <q-card-separator />
-      <div class="row tx-rows-title text-italic" style="background-color: #dadada;">
-        <div class="offset-1"></div>
-        <div class="col-2">Prd</div>
-        <div class="col-4">Description</div>
-        <div class="col-1">Qty</div>
-        <div class="col-2">Price</div>
-        <div class="col-1">Tax</div>
-      </div>
-      <div class="row tx-row" v-for="item in selectedTransactionItems(tx)">
-        <div class="offset-1"></div>
-        <div class="col-2">{{item.productTypeDesc}}</div>
-        <div class="col-4">{{item.productItemDesc}}</div>
-        <div class="col-1">{{item.qty}}</div>
-        <div class="col-2">{{item.price}}</div>
-        <div class="col-1">{{item.tax}}</div>
-      </div>
+    <div v-if="page == 'transactions'" v-for="tx in tfpData.transactions" v-bind:currTx="tx" :key="tx.id" id="transactions">
+      <q-card class="row">
+        <q-card inline class="col-3" flat>
+          <q-card-main>
+            <div class="row">
+              <span class="col-4">Tx#</span>
+              <span class="col-3">{{tx.id}}</span>
+              <span class="col-1">@</span>
+              <span class="col">{{('0' + new Date(tx.txTime).getHours()).slice(-2) + ':' + ('0' + new Date(tx.txTime).getMinutes()).slice(-2)}}</span>
+            </div>
+            <div class="row">
+              <span class="col-4">Tx Total</span>
+              <span class="col">{{tx.total}}</span>
+            </div>
+            <div class="row">
+              <span class="offset-3" />
+              <span class="col-1">+</span>
+              <span id="tx-tax" class="col">{{tx.tax}}</span>
+            </div>
+            <div class="row">
+              <span class="offset-3" />
+              <span class="col-1">=</span>
+              <span id="tx-total" class="col">{{tx.total + tx.tax}}</span>
+            </div>
+            <div class="row">
+              <span class="col">{{tx.pp_or_pl}}</span>
+            </div>
+          </q-card-main>
+        </q-card>
+
+        <q-card inline class="col-8" flat>
+          <q-card  class="row" v-for="item in selectedTransactionItems(tx)" v-bind:data="item" v-bind:key="item.productItemDesc">
+            <q-card-main class="col-12">
+              <div class="row">
+                <span class="col-2">Prd</span>
+                <span class="col-3">{{item.productTypeDesc}}</span>
+              </div>
+              <div class="row">
+                <span class="col-2">Description</span>
+                <span class="col-4">{{item.productItemDesc}}</span>
+              </div>
+              <div class="row">
+                <span class="col-2">Qty</span>
+                <span class="col-3">{{item.qty}}</span>
+              </div>
+              <div class="row">
+                <span class="col-2">Price</span>
+                <span id="tx-total" class="col-2">{{item.price}}</span>
+              </div>
+              <div class="row">
+                <span class="col-2">Tax</span>
+                <span id="tx-total" class="col-2">{{item.tax}}</span>
+              </div>
+              <div class="row">
+                <q-card-separator />
+              </div>
+            </q-card-main>
+          </q-card>
+        </q-card>
+      </q-card>
     </div>
-  </q-card>
   </div>
 </template>
+
 <script>
   import {
     QCard,
+    QCardActions,
     QCardMain,
-    QCardSeparator
+    QCardMedia,
+    QCardSeparator,
+    QCardTitle
   } from 'quasar'
 
   export default {
     components: {
       QCard,
+      QCardActions,
       QCardMain,
-      QCardSeparator
+      QCardMedia,
+      QCardSeparator,
+      QCardTitle
     },
 
     props: ['page', 'tfpData'],
