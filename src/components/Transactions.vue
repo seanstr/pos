16b62382bb44d8e3d40b8b34fa8908dcc443ba79
Sean Matthews <seanstr@gmail.com>
2017-08-30 14:44:33 -0600

    transaction formatting

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -2,26 +2,26 @@
   <div>
     <div v-if="page == 'transactions'" v-for="tx in tfpData.transactions" :key="tx.id" id="transactions">
       <q-card class="row">
-        <q-card inline class="col-3" flat :color="transactionColor">
+        <q-card inline class="col-3 text-dark" flat :color="transactionColor">
           <q-card-main>
             <div class="row">
-              <span class="col-4">Tx#</span>
+              <span class="col-4 text-bold">Tx#</span>
               <span class="col-3">{{tx.id}}</span>
-              <span class="col-1">@</span>
+              <span class="col-1 text-bold">@</span>
               <span class="col">{{('0' + new Date(tx.txTime).getHours()).slice(-2) + ':' + ('0' + new Date(tx.txTime).getMinutes()).slice(-2)}}</span>
             </div>
             <div class="row">
-              <span class="col-4">Tx Total</span>
+              <span class="col-4 text-bold">Tx Total</span>
               <span class="col">{{tx.total}}</span>
             </div>
             <div v-if="tx.pp_or_pl == 'pl'" class="row">
               <span class="offset-3" />
-              <span class="col-1">+</span>
+              <span class="col-1 text-bold">+</span>
               <span id="tx-tax" class="col">{{tx.tax}}</span>
             </div>
             <div v-if="tx.pp_or_pl == 'pl'" class="row">
               <span class="offset-3" />
-              <span class="col-1">=</span>
+              <span class="col-1 text-bold">=</span>
               <span id="tx-total" class="col">{{tx.total + tx.tax}}</span>
             </div>
             <div class="row">
@@ -34,20 +34,20 @@
           <q-card  class="row" v-for="item in selectedTransactionItems(tx)" v-bind:data="item" v-bind:key="item.productItemDesc">
             <q-card-main class="col-12">
               <div class="row">
-                <span class="col-2">Product</span>
-                <span class="col-3">{{item.productTypeDesc}} - {{item.productItemDesc}}</span>
+                <span class="col-2 text-bold">Product</span>
+                <span class="col-10">{{item.productTypeDesc}} - {{item.productItemDesc}}</span>
               </div>
               <div class="row">
-                <span class="col-2">Qty</span>
-                <span class="col-3">{{item.qty}}</span>
+                <span class="col-2 text-bold">Qty</span>
+                <span class="col-10">{{item.qty}}</span>
               </div>
               <div class="row">
-                <span class="col-2">Price</span>
-                <span id="tx-total" class="col-2">{{item.price}}</span>
+                <span class="col-2 text-bold">Price</span>
+                <span id="tx-total" class="col-10">{{item.price}}</span>
               </div>
               <div v-if="tx.pp_or_pl == 'pl'" class="row">
-                <span class="col-2">Tax</span>
-                <span id="tx-total" class="col-2">{{item.tax}}</span>
+                <span class="col-2 text-bold">Tax</span>
+                <span id="tx-total" class="col-10">{{item.tax}}</span>
               </div>
               <div class="row">
                 <q-card-separator />
@@ -90,9 +90,8 @@
 
     computed: {
       transactionColor () {
-        // alert(JSON.stringify(this.currTx))
-        if (this.currTx.pp_or_pl === 'pl') return 'red'
-        return 'green'
+        if (this.currTx.pp_or_pl === 'pl') return 'blue-2'
+        return 'green-2'
       }
     },
 
@@ -107,7 +106,6 @@
             _filtered[key] = _obj[key]
           }
         })
-        // alert(JSON.stringify(_filtered, null, 4))
         return _filtered
       }
     }
